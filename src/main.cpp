b8f308f9e16c3f566448b4dbc98fdc30e6b0572a
arvinmanoj15 <arvinmanoj.k@gmail.com>
2024-06-20 11:03:37 -0400

    vF

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include "server.h"
 
 
+// Main Function
 int main()
 {
-	TCPServer server(12345, "sensor_data.txt");
+	// Create TCPServer object
+	TCPServer server(12345, "sensor_data.txt"); // Creating a server that will listen to port "12345" and use "sensor_data.txt" as sensor data file
+	
+	// Start the server
 	server.run();
+	
 	return 0;
 }
